f1eca640a91961307932357ab0fa2a5e44b40a82
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-07-18 10:26:01 +0200

    renewed project

--- book-network/src/main/java/com/alibou/book/book/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alibou.book.book;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
-}
