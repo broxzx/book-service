32505a9c39f132b49f4c2728ec15f05a23f99ed5
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-07-18 10:23:36 +0200

    renewed project

--- backend/src/main/java/com/project/booksocialnetwork/feedback/FeedbackController.java
+++
@@ -1,9 +0,0 @@
-package com.project.booksocialnetwork.feedback;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/feedbacks")
-public class FeedbackController {
-}
--- backend/src/main/java/com/project/booksocialnetwork/users/data/enums/UserRoles.java
+++
@@ -1,7 +0,0 @@
-package com.project.booksocialnetwork.users.data.enums;
-
-public enum UserRoles {
-
-    ROLE_USER, ROLE_ADMIN
-
-}
